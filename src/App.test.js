ddc39a4464e1f70986797fef892e48294b728537
drespana <drespana@google.com>
2024-04-01 11:11:05 -0500

    all but one passing tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 /** @jest-environment jsdom */
 import React from 'react';
 import '@testing-library/jest-dom';
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen, fireEvent, queryByTestId } from '@testing-library/react';
 import { App } from './App';
 
 /**
@@ -14,8 +14,10 @@ test('App should render', () => {
 });
 
 test('Button should render', () => {
+  render(<App/>)
+  
   // TODO: change the expect to actually test something 😉
-  expect('no test written').toBe('tested');
+  expect(screen.findByRole('button')).toBeTruthy();
 });
 
 /**
@@ -23,16 +25,21 @@ test('Button should render', () => {
  * hint: use fireEvent.click(element) to trigger a click event on an element
  */
 test('theme button should update button text', () => {
+  render(<App/>)
+  fireEvent.click(screen.getByText('Current theme: light', {selector:'button'}))
+
   // TODO: change the expect to actually test something 😉
-  expect('no test written').toBe('tested');
+  expect(screen.findByRole('button', {selector:'Current theme: dark'})).toBeTruthy();
 });
 
 // BONUS
 // hint: there is a `.toHaveStyle` method.
 // e.g.: expect(element).toHaveStyle('color: #FFF');
 test('theme button should toggle styles', () => {
+  render(<App/>)
+  fireEvent.click(screen.getByText('Current theme: light', {selector:'button'}))
   // TODO: change the expect to actually test something 😉
-  expect('no test written').toBe('tested');
+  expect(queryByTestId(document.body.style.color)).toBe('rgb(51, 51, 51)');
 });
 
 /**
@@ -44,9 +51,12 @@ test('theme button should toggle styles', () => {
  * hint: use `queryByText` instead of `getByText` to check if something is _not_ rendered
  * (getByText will throw an error if it is not rendered)
  */
-test('hidden button should toggle hidden content', () => {
+test('hidden button should toggle hidden content', async () => {
   // TODO: change the expect to actually test something 😉
-  expect('no test written').toBe('tested');
+  render(<App/>)
+  fireEvent.click(screen.getByText('Show hidden content', {selector:'button'}))
+
+  expect(screen.getByText('this content is hidden by default')).toBeTruthy();
 });
 
 
